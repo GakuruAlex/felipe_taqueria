911773fa1e4268dc06fdb25e5f0829a5eb593772
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-04 11:29:01 +0300

    test ing calculate price

--- test_felipe_taqueria.py
+++ test_felipe_taqueria.py
@@ -4,10 +4,21 @@ from felipe_taqueria import FelipeTaqueria
 @pytest.mark.parametrize("item, price", [
     ("bowl", 8.50),
     ("baja taco", 4.25),
-    ("Burrito, 7.50"),
+    ("Burrito", 7.50),
     ("Quesadilla", 8.50)
 ])
 class TestGetPrice:
     felipe_taqueria = FelipeTaqueria()
     def test_get_price(self, item , price):
         assert self.felipe_taqueria.get_price(item) == price
+
+@pytest.mark.parametrize("price, total",[
+    (8.50, 8.50),
+    (4.25, 12.75),
+    (7.50, 20.25),
+    (8.50, 28.75)
+])
+class TestCalculatePrice:
+    felipe_taqueria = FelipeTaqueria()
+    def test_calculate_price(self, price, total):
+        assert self.felipe_taqueria.calculate_total(price) == total
